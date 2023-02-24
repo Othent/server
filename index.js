b08f873eaac532afc7a01f9e416f2ff78bffc66e
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-02-24 13:11:03 +0000

    -

--- index.js
+++ index.js
@@ -1,6 +1,45 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+
+const Arweave = require('arweave');
+async function uploadFileToArweave(base64Data, contentType) {
+    // Initialize Arweave client
+    const arweave = Arweave.init({
+      host: 'arweave.net',
+      port: 443,
+      protocol: 'https'
+    });
+  
+    // Decode the file data from Base64 to a Buffer object
+    const fileData = Buffer.from(base64Data, 'base64');
+  
+    // Read the JWK wallet key from wallet.json
+    const walletData = process.env.wallet;
+    const wallet = JSON.parse(walletData);
+  
+    // Create a transaction object
+    const transaction = await arweave.createTransaction({
+      data: fileData
+    }, wallet);
+  
+    // Set the content type
+    if (contentType) {
+      transaction.addTag('Content-Type', contentType);
+    }
+  
+    // Sign and submit the transaction
+    await arweave.transactions.sign(transaction, wallet);
+    await arweave.transactions.post(transaction);
+  
+    return `File uploaded to Arweave with transaction ID: ${transaction.id}`;
+  }
+  
+
+
+
+
+
 const app = express();
 app.use(bodyParser.json());
 
@@ -9,28 +48,30 @@ app.get('/', (req, res) => {
     res.json({ hello: 'world' });
 });
 
-const uploadFileToArweave = require('./upload.js')
+
+
+
 
 // Define a route to handle file uploads
 app.post('/upload', (req, res) => {
     const { name, data } = req.body;
     const buffer = Buffer.from(data, 'base64');
-
-    res.json({buffer: buffer})
-})
   
-//     // Call the uploadFileToArweave function with the specified buffer and content type
-//     uploadFileToArweave(buffer, 'image/png')
-//       .then(transactionId => {
-//         res.json({ success: true, transactionId });
-//       })
-//       .catch(error => {
-//         console.error(error);
-//         res.status(500).json({ success: false, error: error.message });
-//       });
-//   });
+    // Call the uploadFileToArweave function with the specified buffer and content type
+    uploadFileToArweave(buffer, 'image/png')
+      .then(transactionId => {
+        res.json({ success: true, transactionId });
+      })
+      .catch(error => {
+        console.error(error);
+        res.status(500).json({ success: false, error: error.message });
+      });
+  });
 
 
+
+
+  
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
