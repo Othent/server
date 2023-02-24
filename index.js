824ebef3c8d314fd4a574251520e14eb73f06b8a
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-02-24 12:07:46 +0000

    -

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
 
+const app = express();
+app.use(bodyParser.json());
 
-const uploadFileToArweave = require('./upload');
+app.post('/', (req, res) => {
+  console.log('Received POST request');
+  // Add your function call here
+  res.json({ hello: 'world' });
+});
 
-uploadFileToArweave('file.png', 'image/png');
+app.get('/', (req, res) => {
+  res.json({ hello: 'world' });
+});
 
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
