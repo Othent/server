026547cda1ad753981773ceb41dcd7eb8d8d7f4e
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-02-24 12:23:05 +0000

    -

--- index.js
+++ index.js
@@ -4,15 +4,19 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
+
+
 app.post('/upload', (req, res) => {
-  console.log('Received POST request');
   // Add your function call here
   res.json({ Tate: 'Berenbaum' });
 });
 
+
 app.get('/', (req, res) => {
   res.json({ hello: 'world' });
 });
+
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
