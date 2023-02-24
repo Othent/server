90d1d87dd41b413a13b7279d050a82a35d0298f4
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-02-24 12:59:04 +0000

    -

--- upload.js
+++ upload.js
@@ -1,8 +1,6 @@
 const Arweave = require('arweave');
 
-
-
-async function uploadFileToArweave(fileData, contentType) {
+async function uploadFileToArweave(base64Data, contentType) {
   // Initialize Arweave client
   const arweave = Arweave.init({
     host: 'arweave.net',
@@ -10,16 +8,16 @@ async function uploadFileToArweave(fileData, contentType) {
     protocol: 'https'
   });
 
-  // Encode the file data as a Base64 string
-  const base64Data = fileData.toString('base64');
+  // Decode the file data from Base64 to a Buffer object
+  const fileData = Buffer.from(base64Data, 'base64');
 
   // Read the JWK wallet key from wallet.json
-  const walletData = process.env.wallet
+  const walletData = process.env.wallet;
   const wallet = JSON.parse(walletData);
 
   // Create a transaction object
   const transaction = await arweave.createTransaction({
-    data: base64Data
+    data: fileData
   }, wallet);
 
   // Set the content type
@@ -31,8 +29,7 @@ async function uploadFileToArweave(fileData, contentType) {
   await arweave.transactions.sign(transaction, wallet);
   await arweave.transactions.post(transaction);
 
-  return `File uploaded to Arweave with transaction ID: ${transaction.id}`
+  return `File uploaded to Arweave with transaction ID: ${transaction.id}`;
 }
-
 
 module.exports = uploadFileToArweave;
