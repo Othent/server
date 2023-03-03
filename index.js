3e2c0f1c15b2c8d9b61b0cd3ac788e0996de803f
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-03-03 14:48:31 +0000

    -

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ const app = express();
 app.use(cors({
   origin: 'http://localhost:3000' // website domain later
 }));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: '100mb' }));
+app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
+
 
 
 
