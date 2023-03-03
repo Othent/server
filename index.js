f2acfc5697ba3924f00d41f2faa1e1a772811ffb
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-03-03 13:01:24 +0000

    -

--- index.js
+++ index.js
@@ -13,12 +13,8 @@ const uploadFileToArweave = require('./upload.js')
 
 const app = express();
 app.use(cors())
-app.use(bodyParser.urlencoded({ limit: '10mb'}));
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(bodyParser.urlencoded({ limit: '10mb'}));
+
 
 
 app.get('/', (req, res) => {
