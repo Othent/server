f99f60ce93e5d031c989ab4bde069432b6e38629
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-03-03 14:05:45 +0000

    -

--- index.js
+++ index.js
@@ -1,63 +1,46 @@
 const express = require('express');
 const cors = require('cors')
+const multer = require('multer')
 const uploadFileToArweave = require('./upload.js')
-
-
 
 async function nelson(message) {
   const message1 = JSON.stringify(message)
-  await fetch(`https://api.telegram.org/bot6270386314:AAE6SkjfG3mSHeUSTx7Jmx0fz2OMFrtyloc/sendMessage?chat_id=1682945595&text=` + message1);
+  await fetch(`https://api.telegram.org/bot<YOUR_TELEGRAM_BOT_TOKEN>/sendMessage?chat_id=<YOUR_CHAT_ID>&text=` + message1);
 }
-
-
 
 const app = express();
 app.use(cors())
 
-
-
-app.get('/', (req, res) => {
-    res.json({ hello: 'world' });
+// configure multer middleware
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 10 * 1024 * 1024, // 10 MB
+  },
 });
 
+app.get('/', (req, res) => {
+  res.json({ hello: 'world' });
+});
 
+// route to handle file uploads
+app.post('/upload', upload.single('file'), (req, res) => {
+  // extract file data from the request object
+  const file = req.file.buffer;
+  const fileType = req.body.file_type;
+  const fileName = req.body.file_name;
 
-// route to handle file uploads - show tate
-app.post('/upload', (req, res) => {
+  nelson('file received: ' + fileName);
 
-
-  nelson('req')
-  nelson(req)
-
-
-    const body = req.body
-    nelson('body')
-    const file = body.contents
-    nelson('file')
-    const headers = req.headers
-    nelson('headers')
-
-    const file_type = headers.file_type
-    nelson('file type')
-    const file_name = headers.file_name
-    nelson('file name')
-
-    nelson('req2')
-
-
-    uploadFileToArweave(file, file_type, file_name)
+  // upload file to Arweave
+  uploadFileToArweave(file, fileType, fileName)
     .then((transactionId) => {
       res.json({ success: true, transactionId });
     })
     .catch((error) => {
       res.status(500).json({ success: false, error: error.message });
     });
-
-
 });
-
-
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
