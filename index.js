54d66a57f3b1f898a29ec3b518e4a555844c8138
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-03-03 16:18:24 +0000

    -

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const uploadFileToArweave = require('./upload.js');
 
 const app = express();
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://weavetransfer.com',
 }));
 app.use(bodyParser.json({ limit: '100mb' }));
 app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
