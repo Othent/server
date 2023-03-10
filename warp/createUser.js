986ca826e3bdb4d915a77b473817499f5dc5bd8f
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-03-10 16:01:09 +0000

    -

--- warp/createUser.js
+++ warp/createUser.js
@@ -7,8 +7,8 @@ import jwt from 'jsonwebtoken'
 export default async function createUser(JWT) {
 
     const wallet = await configureWallet()
-    const contract_state = fs.readFileSync('state.json', 'utf-8')
-    const contractCode = fs.readFileSync('contract.js', 'utf-8')
+    const contract_state = fs.readFileSync('./state.json', 'utf-8')
+    const contractCode = fs.readFileSync('./contract.js', 'utf-8')
 
     let wallet_contract = await warp.createContract.deploy({
         wallet, 
