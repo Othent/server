e2167e9d1d63322704ec5dc6227e40c91c10620b
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-03-20 11:15:51 +0000

    -

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(cors({
 }));
 app.use(bodyParser.json({ limit: '100mb' }));
 app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
-
+app.set('title', 'Your website name here');
 
 
 // Home
