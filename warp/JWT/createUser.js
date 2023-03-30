f6ebe5dfde015243359cac79100e23c5fc97a84f
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-03-30 10:16:29 +0100

    -

--- warp/JWT/createUser.js
+++ warp/JWT/createUser.js
@@ -1,56 +1,67 @@
 import { warp, configureWallet } from '../warp-configs.js'
 import updateDB from '../../EXM/updateDB.js'
 import jwt from 'jsonwebtoken'
+import queryDB from '../../EXM/queryDB.js'
 
-// if user not in query DB then update it, else return ID and a message
+
 
 export default async function createUser(JWT) { 
 
-    const wallet = await configureWallet()
-    const contract_state = { 
-        App: "Othent.io", 
-        Description: "Merging Web2 to Web3 user logins with a familiar and simple interface",
-        user_id: null, 
-        contract_address: null,
-        last_nonce: null,
-        JWK_public_key: null
-    }
-    let contract_code = await fetch('https://othent.io/contract.txt')
-    contract_code = await contract_code.text();
-
-
-    const createOptions = {tags: [
-        {name: "Contract-App", value: "Othent.io"}, 
-        {name: "Contract-Description", value: "Othent.io merges Web2 to Web3 user logins with a familiar and simple interface"}, 
-    ]};
-    let tags = createOptions.tags
-    const { contractTxId } = await warp.deploy({
-        wallet: wallet, 
-        initState: JSON.stringify(contract_state), 
-        src: contract_code,
-        tags
-      });
-
-    const contract = warp.contract(contractTxId).connect(wallet.jwk).setEvaluationOptions({internalWrites: true})
-
-    const writeOptions = {tags: [
-        {name: "Contract-App", value: "Othent.io"}, 
-        {name: "Function", value: "initializeContract"}
-    ]};
-
-    await contract.writeInteraction({
-        function: 'initializeContract',
-        jwt: JWT,
-        contract_address: contractTxId,
-        encryption_type: "JWT"
-    }, writeOptions)
- 
-
-
     const unique_ID = jwt.decode(JWT).sub
-    await updateDB(unique_ID, contractTxId)
+    const checkUser = await queryDB(unique_ID, contractTxId)
+
+    if (checkUser.response !== 'user not found') {
+
+        const wallet = await configureWallet()
+        const contract_state = { 
+            App: "Othent.io", 
+            Description: "Merging Web2 to Web3 user logins with a familiar and simple interface",
+            user_id: null, 
+            contract_address: null,
+            last_nonce: null,
+            JWK_public_key: null
+        }
+        let contract_code = await fetch('https://othent.io/contract.txt')
+        contract_code = await contract_code.text();
 
 
-    return contractTxId
+        const createOptions = {tags: [
+            {name: "Contract-App", value: "Othent.io"}, 
+            {name: "Contract-Description", value: "Othent.io merges Web2 to Web3 user logins with a familiar and simple interface"}, 
+        ]};
+        let tags = createOptions.tags
+        const { contractTxId } = await warp.deploy({
+            wallet: wallet, 
+            initState: JSON.stringify(contract_state), 
+            src: contract_code,
+            tags
+        });
+
+        const contract = warp.contract(contractTxId).connect(wallet.jwk).setEvaluationOptions({internalWrites: true})
+
+        const writeOptions = {tags: [
+            {name: "Contract-App", value: "Othent.io"}, 
+            {name: "Function", value: "initializeContract"}
+        ]};
+
+        await contract.writeInteraction({
+            function: 'initializeContract',
+            jwt: JWT,
+            contract_address: contractTxId,
+            encryption_type: "JWT"
+        }, writeOptions)
+    
+
+
+        await updateDB(unique_ID, contractTxId)
+
+        return contractTxId
+
+    }
+
+    else {
+        throw new Error(checkUser);
+    }
     
 }
+
