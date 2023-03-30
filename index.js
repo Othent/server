977b80bae9095f2a2213029aa96b2a695426d831
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-03-30 10:22:30 +0100

    -

--- index.js
+++ index.js
@@ -47,9 +47,11 @@ app.post('/create-user', (req, res) => {
   const JWT = req.body.JWT;
   createUser(JWT)
     .then((response) => {
+      console.log(1234)
       res.json({ success: true, contract_id: response});
     })
     .catch((error) => {
+      console.log(4321)
       res.status(500).json({ success: false, error: error.message });
     });
 });
