0f624dc93f1f348863188cdb33d68387cf8c7678
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-03-31 12:44:21 +0100

    -

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ app.post('/query-user', (req, res) => {
 // Upload data - arweave
 import uploadFileToArweave from './arweave/upload.js';
 app.post('/upload-data', upload.single('file'), (req, res) => {
-  console.log(req)
+  console.log(req.file)
   const file = req.file;
   uploadFileToArweave(file)
     .then((transaction_id) => {
