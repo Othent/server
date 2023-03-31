1ea6659b49b5f72839a524f4fecc063007d5636c
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-03-31 14:21:47 +0100

    -

--- warp/JWK/JWKBackupTxn.js
+++ warp/JWK/JWKBackupTxn.js
@@ -3,9 +3,11 @@ import queryDB from '../../EXM/queryDB.js'
 
 // this does not work / hasnt been checked
 
-export default async function JWKBackupTxn(PEM_key_JWT) {
+export default async function JWKBackupTxn(JWT) {
 
-    const contract_id = await queryDB(PEM_key_JWT); // make sure client id is in this
+
+    // query diff DB (same) for the JWK public keys to contract IDs
+    const contract_id = await queryDB(JWT); // make sure client id is in this
 
     const wallet = await configureWallet()
     const contract = warp.contract(contract_id.wallet_contract).setEvaluationOptions({internalWrites: true}).connect(wallet.jwk)
@@ -16,7 +18,7 @@ export default async function JWKBackupTxn(PEM_key_JWT) {
 
     const transaction_id = await contract.writeInteraction({
         function: 'JWKBackupTxn',
-        jwt: PEM_key_JWT,
+        jwt: JWT,
         encryption_type: 'JWK'
     }, options)
 
