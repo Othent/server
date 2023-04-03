b0595aab13d6666d8d4b321e45073869bfb2421d
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-04-03 11:32:58 +0100

    -

--- database/queryDB.js
+++ database/queryDB.js
@@ -3,13 +3,13 @@ import jwt from 'jsonwebtoken';
 
 export default async function queryDB(JWT) {
   
-  const contract_address = jwt.decode(JWT).contract_address
+  const contract_id = jwt.decode(JWT).contract_id
   const unique_id = jwt.decode(JWT).sub
 
-  console.log(contract_address)
+  console.log(contract_id)
 
-  if (contract_address) {
-    return {wallet_contract: contract_address, unique_ID: unique_id}
+  if (contract_id) {
+    return {wallet_contract: contract_id, unique_ID: unique_id}
 
   } else {
     return {'response': 'user not found'}
