bacf4ef3f50ff19cc832822d3338785d750ff972
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-04-06 12:25:08 +0100

    return user details

--- warp/JWT/createUser.js
+++ warp/JWT/createUser.js
@@ -3,6 +3,7 @@ import updateDB from '../../database/updateDB.js'
 import queryDB from '../../database/queryDB.js'
 import jwt from 'jsonwebtoken';
 import sendEmail from '../../new_user_email/email.js'
+import axios from 'axios'
 
 
 
@@ -55,9 +56,43 @@ export default async function createUser(JWT) {
 
         const decoded_JWT = jwt.decode(JWT)
         await updateDB(decoded_JWT.sub, contractTxId)
-        await sendEmail(decoded_JWT.email, contractTxId)
 
-        return contractTxId
+
+        var auth0Domain = process.env.auth0Domain;
+        var auth0ClientId = process.env.auth0ClientId;
+        var auth0ClientSecret = process.env.auth0ClientSecret;
+        var audience = 'https://' + auth0Domain + '/api/v2/';
+        var tokenUrl = 'https://' + auth0Domain + '/oauth/token';
+        var tokenParams = { grant_type: 'client_credentials', client_id: auth0ClientId, client_secret: auth0ClientSecret, audience: audience };
+        await axios.post(tokenUrl, tokenParams)
+        .then(async function (response) {
+            var token = response.data.access_token;
+
+            var options = {
+                method: 'GET',
+                url: 'https://othent.us.auth0.com/api/v2/users/' + decoded_JWT.sub,
+                headers: {authorization: 'Bearer ' + token, 'content-type': 'application/json'},
+                data: {}
+            };
+            
+            axios.request(options).then( async function (response) {
+                const user_data = response.data
+                await sendEmail(user_data.email, contractTxId)
+                return {
+                email: user_data.email, email_verified: user_data.email_verified, family_name: user_data.family_name,
+                given_name: user_data.given_name, locale: user_data.locale, name: user_data.name, nickname: user_data.nickname,
+                picture: user_data.picture, user_id: user_data.user_id, contract_id: contractTxId
+                }
+            }).catch(function (error) {
+                console.error(error);
+            });
+
+        })
+
+
+
+
+
 
     }
 
