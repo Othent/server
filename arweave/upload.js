97e8698b1709d65138b872f9cb4c6796a290ae37
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-04-10 14:39:37 +0100

    -

--- arweave/upload.js
+++ arweave/upload.js
@@ -1,4 +1,5 @@
 import Arweave from 'arweave';
+import jwt from 'jsonwebtoken';
 
 
 export default async function uploadFileToArweave(file, fileHashJWT) {
@@ -22,6 +23,16 @@ export default async function uploadFileToArweave(file, fileHashJWT) {
   transaction.addTag('Content-Type', file.mimetype);
   transaction.addTag('File-Name', file.originalname);
   transaction.addTag('File-Hash-JWT', fileHashJWT);
+
+  function addTagsToTransaction(transaction, tags) {
+    for (let i = 0; i < tags.length; i++) {
+      const tag = tags[i];
+      transaction.addTag(tag.name, tag.value);
+    }
+  }
+
+  const tags = jwt.decode(fileHashJWT).tags
+  addTagsToTransaction(transaction, tags)
 
 
   await arweave.transactions.sign(transaction, wallet);
--- warp/JWT/sendTransaction.js
+++ warp/JWT/sendTransaction.js
@@ -1,16 +1,16 @@
 import { warp, configureWallet } from '../warp-configs.js'
 import queryDB from '../../database/queryDB.js'
+import jwt from 'jsonwebtoken';
 
 export default async function sendTransaction(JWT) {
 
     const contract_id = await queryDB(JWT);
-
     const wallet = await configureWallet()
     const contract = warp.contract(contract_id.contract_id).setEvaluationOptions({internalWrites: true}).connect(wallet.jwk)
-    const options = {tags: [
-        {name: "Contract-App", value: "Othent.io"}, 
-        {name: "Function", value: "sendTransaction"}
-    ]};
+    
+    let tags = jwt.decode(JWT).tags
+    tags.push( {name: "Contract-App", value: "Othent.io"}, {name: "Function", value: "sendTransaction"} )
+    const options = {tags};
 
     const transaction_id = await contract.writeInteraction({
         function: 'sendTransaction',
