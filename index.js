fdb2032be9eab1b642fc7693964ab946a1098cc1
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-04-12 12:43:27 +0100

    -

--- index.js
+++ index.js
@@ -89,7 +89,6 @@ import uploadFileToArweave from './arweave/upload.js';
 app.post('/upload-data', upload.single('file'), (req, res) => {
   const file = req.file;
   const fileHashJWT = req.body.fileHashJWT
-  console.log(file, fileHashJWT)
   uploadFileToArweave(file, fileHashJWT)
     .then((transaction_id) => {
       res.json({ success: true, transactionId: transaction_id });
