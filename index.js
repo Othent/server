26070be045ee9d947bb38d71eba899d0b859dfbb
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-04-17 21:31:58 +0100

    -

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors({
 app.use(bodyParser.json({ limit: '100mb' }));
 app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
 import multer from 'multer';
-const upload = multer();
+const upload = multer({ dest: 'uploads/' });
 
 
 
