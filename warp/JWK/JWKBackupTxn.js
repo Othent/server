8ff4700ddd171b4a7463680a28c1935b4061577e
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-04-18 00:10:17 +0100

    -

--- warp/JWK/JWKBackupTxn.js
+++ warp/JWK/JWKBackupTxn.js
@@ -4,27 +4,30 @@ import jwt from 'jsonwebtoken';
 
 
 export default async function JWKBackupTxn(JWK_signed_JWT) {
+    try {
+        const state = (await readContract(JWK_signed_JWT)).state
+        const current_nonce = jwt.decode(JWK_signed_JWT).iat
 
-    const state = (await readContract(JWK_signed_JWT)).state
-    const current_nonce = jwt.decode(JWK_signed_JWT).iat
+        if (state.last_nonce < current_nonce) {
+            const wallet = await configureWallet()
+            const contract = warp.contract(state.contract_address).setEvaluationOptions({internalWrites: true}).connect(wallet.jwk)
+            const options = {tags: [
+                {name: "Contract-App", value: "Othent.io"}, 
+                {name: "Function", value: "JWKBackupTxn"}
+            ]};
 
-    if (state.last_nonce < current_nonce) {
-        const wallet = await configureWallet()
-        const contract = warp.contract(state.contract_address).setEvaluationOptions({internalWrites: true}).connect(wallet.jwk)
-        const options = {tags: [
-            {name: "Contract-App", value: "Othent.io"}, 
-            {name: "Function", value: "JWKBackupTxn"}
-        ]};
+            const transaction_id = await contract.writeInteraction({
+                function: 'JWKBackupTxn',
+                jwt: JWK_signed_JWT,
+                encryption_type: 'JWK'
+            }, options)
 
-        const transaction_id = await contract.writeInteraction({
-            function: 'JWKBackupTxn',
-            jwt: JWK_signed_JWT,
-            encryption_type: 'JWK'
-        }, options)
-
-        return { success: true, transaction_id: transaction_id.originalTxId }
-    } else return { success: false, message: 'Invalid nonce' }
-
+            return { success: true, transaction_id: transaction_id.originalTxId }
+        } else {
+            return { success: false, message: 'Invalid nonce' }
+        }
+    } catch (error) {
+        throw new Error(`JWK transaction error: ${error.message}`)
+    }
 }
-
 
