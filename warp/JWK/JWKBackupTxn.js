6d21da24f976e6f82f96a0def64a498fa3f07a89
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-04-18 10:57:16 +0100

    -

--- warp/JWK/JWKBackupTxn.js
+++ warp/JWK/JWKBackupTxn.js
@@ -23,7 +23,7 @@ export default async function JWKBackupTxn(JWK_signed_JWT) {
                 encryption_type: 'JWK'
             }, options)
 
-            console.log('transaction_id', transaction_id)
+            console.log('STATETETETETETETETTE', transaction_id.state.errors)
 
             return { success: true, transaction_id: transaction_id.originalTxId }
 
