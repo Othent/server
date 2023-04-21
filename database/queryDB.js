bbf9f3ca93c337ea39dffec765ebd334a47c7494
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-04-21 12:31:43 +0100

    -

--- database/queryDB.js
+++ database/queryDB.js
@@ -4,9 +4,9 @@ import jwt from 'jsonwebtoken';
 export default async function queryDB(JWT) {
   
   const contract_id = jwt.decode(JWT).contract_id
-  const unique_id = jwt.decode(JWT).sub
 
   if (contract_id) {
+    const unique_id = jwt.decode(JWT).sub
     return {contract_id: contract_id, unique_ID: unique_id}
 
   } else {
