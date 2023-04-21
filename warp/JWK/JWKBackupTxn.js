e31329d52cf6b330aa9a8ff8f4e7c7d30090cd38
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-04-21 12:20:27 +0100

    feat: jwk txn please create othent account

--- warp/JWK/JWKBackupTxn.js
+++ warp/JWK/JWKBackupTxn.js
@@ -1,64 +1,66 @@
 import { warp, configureWallet } from '../warp-configs.js'
 import readContract from '../readContract.js';
 import jwt from 'jsonwebtoken';
+import queryDB from '../../database/queryDB.js';
 
 
 export default async function JWKBackupTxn(JWK_signed_JWT) {
-    try {
 
-        const current_state = (await readContract(JWK_signed_JWT)).state
+    const checkDB = await queryDB(JWK_signed_JWT)
+    if (checkDB.response === 'user not found') {
+        return {success: false, message: 'Please create a Othent account'}
+    }
 
-        const decoded_JWT = jwt.decode(JWK_signed_JWT)
+    const current_state = (await readContract(JWK_signed_JWT)).state
 
-        const current_nonce = decoded_JWT.iat
+    const decoded_JWT = jwt.decode(JWK_signed_JWT)
 
-        
+    const current_nonce = decoded_JWT.iat
 
-        if (current_state.JWK_public_key === null) {
+    
 
-            if (current_state.last_nonce < current_nonce) {
+    if (current_state.JWK_public_key === null) {
 
-                const wallet = await configureWallet()
-                const contract = warp.contract(current_state.contract_address).setEvaluationOptions({internalWrites: true}).connect(wallet.jwk)
+        if (current_state.last_nonce < current_nonce) {
 
-                let tags = decoded_JWT.tags
-                tags ??= [];
-                tags.push( {name: "Contract-App", value: "Othent.io"}, {name: "Function", value: "JWKBackupTxn"} )
-                const options = {tags};
+            const wallet = await configureWallet()
+            const contract = warp.contract(current_state.contract_address).setEvaluationOptions({internalWrites: true}).connect(wallet.jwk)
 
-                const othentFunction = decoded_JWT.contract_input.othentFunction
+            let tags = decoded_JWT.tags
+            tags ??= [];
+            tags.push( {name: "Contract-App", value: "Othent.io"}, {name: "Function", value: "JWKBackupTxn"} )
+            const options = {tags};
 
-                const transaction = await contract.writeInteraction({
-                    function: othentFunction,
-                    jwt: JWK_signed_JWT,
-                    encryption_type: 'JWK'
-                }, options)
+            const othentFunction = decoded_JWT.contract_input.othentFunction
 
-                const { cachedValue } = await contract.readState();
-                const { state, validity, errorMessages} = cachedValue
-                const transactionId = transaction.originalTxId
+            const transaction = await contract.writeInteraction({
+                function: othentFunction,
+                jwt: JWK_signed_JWT,
+                encryption_type: 'JWK'
+            }, options)
 
-                if (errorMessages[transactionId]) {
-                    return { success: false, transactionId, bundlrId: transaction.bundlrResponse.id, 
-                        errors: errorMessages[transactionId], state }
-                } else if (errorMessages[transactionId] === undefined) {
-                    return { success: true, transactionId, bundlrId: transaction.bundlrResponse.id, 
-                        errors: {}, state }
-                }
+            const { cachedValue } = await contract.readState();
+            const { state, validity, errorMessages} = cachedValue
+            const transactionId = transaction.originalTxId
 
-            } else {
-                return { success: false, message: 'Invalid nonce' }
+            if (errorMessages[transactionId]) {
+                return { success: false, transactionId, bundlrId: transaction.bundlrResponse.id, 
+                    errors: errorMessages[transactionId], state }
+            } else if (errorMessages[transactionId] === undefined) {
+                return { success: true, transactionId, bundlrId: transaction.bundlrResponse.id, 
+                    errors: {}, state }
             }
-            
 
         } else {
-            return { success: false, message: 'No JWK initialized' }
+            return { success: false, message: 'Invalid nonce' }
         }
+        
 
-    } catch (error) {
-        console.error(`JWK transaction error: ${error.message}`);
-        return { success: false, message: 'Error processing JWK transaction', error: error.message };
+    } else {
+        return { success: false, message: 'No JWK initialized' }
     }
+
+
 }
 
 
