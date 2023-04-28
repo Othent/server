94982702f86f8f5be27b2be1ae30e6729f39df51
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-04-28 20:35:38 +0100

    -

--- database/alert.js
+++ database/alert.js
@@ -3,7 +3,6 @@
 
 export default async function alert(type, details) {
     const chatId = process.env.tg_chat_id
-    console.log(chatId)
 
     let message
     if (type === 'new user') {
@@ -17,7 +16,6 @@ export default async function alert(type, details) {
     const url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatId}&text=${message}`;
     fetch(url)
     .then(response => response.json())
-    .then(data => console.log(data))
     .catch(error => console.error(error));
 
 }
