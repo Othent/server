0994daa93b125aacb5a516d3a4e0f6cd16c63562
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-04-30 17:46:17 +0100

    fix: bundlr upload data

--- bundlr/upload.js
+++ bundlr/upload.js
@@ -25,7 +25,7 @@ export default async function uploadFileToBundlr(data, dataHashJWT) {
     // await bundlr.fund(price);
 
 
-    const transaction = await bundlr.upload(data.buffer);
+    const transaction = await bundlr.upload('data.buffer');
     console.log(`${pathToFile} --> Uploaded to https://arweave.net/${id}`);
 
 
