c2bfd50f8c6db7aadd1cb57f03254e89014e5c7c
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-05-04 20:31:18 -0400

    fix: undef var

--- index.js
+++ index.js
@@ -42,7 +42,6 @@ app.post('/email-list', (req, res) => {
 // File uploads only for weavetransfer
 import weavetransferUpload from './weavetransfer/upload.js';
 app.post('/weavetransfer', upload.single('file'), (req, res) => {
-  console.log(req)
   const transaction_id = req.body.transaction_id;
   const sendFromEmail = req.body.sendFromEmail;
   const sendToEmail = req.body.sendToEmail;
