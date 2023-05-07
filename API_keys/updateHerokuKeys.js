d154f84ef695f422b765bbe7ff675e64f80e1b71
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-05-07 16:34:05 -0400

    -

--- API_keys/updateHerokuKeys.js
+++ API_keys/updateHerokuKeys.js
@@ -1,31 +1,29 @@
-// import Heroku from 'heroku-client';
+import Heroku from 'heroku-client';
 
-// // Othent : ifhhfh38r38fbcj in txn
+// Othent : ifhhfh38r38fbcj in txn
 
-// // if no API keys return false as well as incorrect on package
+// if no API keys return false as well as incorrect on package
 
-// export default async function updateHerokuKeys(API_KEY, API_ID) {
+export default async function updateHerokuKeys(API_KEY, API_ID) {
 
-//   const heroku = new Heroku({ token: process.env.heroku_api_key });
-//   const appName = 'othent-server';
-//   const key = 'API_KEYS';
-//   const existing_API_keys = process.env.API_KEYS ? process.env.API_KEYS.split(',') : [];
+  const heroku = new Heroku({ token: process.env.heroku_api_key });
+  const appName = 'othent-server';
+  const key = 'API_KEYS';
+  const existing_API_keys = process.env.API_KEYS ? process.env.API_KEYS.split(',') : [];
 
-//   console.log('existing_API_keys', existing_API_keys)
+  console.log('existing_API_keys', existing_API_keys)
 
-//   existing_API_keys.push(API_KEY)
-//   console.log('existing_API_keys', existing_API_keys)
-//   const configVars = { [key]: existing_API_keys };
+  existing_API_keys.push(API_KEY)
+  console.log('existing_API_keys', existing_API_keys)
+  const configVars = { [key]: existing_API_keys };
 
-//   heroku.patch(`/apps/${appName}/config-vars`, { body: configVars })
-//   .then(() => {
-//     return true
-//   })
-//   .catch((error) => {
-//     return { success: false, error: error }
-//   });
+  heroku.patch(`/apps/${appName}/config-vars`, { body: configVars })
+  .then(() => {
+    return true
+  })
+  .catch((error) => {
+    return { success: false, error: error }
+  });
   
 
-// }
-
-// updateHerokuKeys('API_KEY', 'API_ID')
+}
