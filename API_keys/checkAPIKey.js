89b62a62df38ea56d49befa653c99e366bf4be34
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-05-07 14:53:17 -0400

    -

--- API_keys/checkAPIKey.js
+++ API_keys/checkAPIKey.js
@@ -2,18 +2,13 @@
 
 export default async function checkAPIKey(API_KEY, API_ID) {
 
-  console.log('Lorimer')
-
   const existing_API_keys = process.env.API_KEYS
-
-  console.log('existing_API_keys', existing_API_keys);
     
   if (existing_API_keys.includes(API_KEY)) {
-    console.log(true)
     return { response: 'ok', success: true }
   } else {
-    console.log(false)
     return { response: 'Invalid API keys / not found - get API keys at Othent.io', success: false }
   }
 
 }
+
