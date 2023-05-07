fffd673c008939b33dca7473f580d08f881b7511
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-05-07 14:36:54 -0400

    fix: json err

--- API_keys/checkAPIKey.js
+++ API_keys/checkAPIKey.js
@@ -4,7 +4,12 @@ export default async function checkAPIKey(API_KEY, API_ID) {
 
   console.log('Lorimer')
 
-  const existing_API_keys = JSON.parse(process.env.API_KEYS || '[]');
+  let h = [{ ID: API_ID, KEY: API_KEY }]
+  console.log('h', h)
+  
+  const value = JSON.stringify(parsedKeys);
+
+  const existing_API_keys = JSON.parse([]);
 
   console.log('existing_API_keys', existing_API_keys);
     
