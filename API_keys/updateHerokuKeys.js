0b2c8a37a025214f4f534fa0319a58fbca03eb3f
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-05-07 16:45:39 -0400

    -

--- API_keys/updateHerokuKeys.js
+++ API_keys/updateHerokuKeys.js
@@ -9,11 +9,7 @@ export default async function updateHerokuKeys(API_KEY, API_ID) {
   const heroku = new Heroku({ token: process.env.heroku_api_key });
   const appName = 'othent-server';
   const key = 'API_KEYS';
-  const existing_API_keys = process.env.API_KEYS ? process.env.API_KEYS.split(',') : [];
-
-  console.log('existing_API_keys', existing_API_keys)
-  console.log(typeof process.env.API_KEYS)
-  console.log('here', JSON.parse(existing_API_keys), typeof JSON.parse(existing_API_keys))
+  const existing_API_keys = JSON.parse(existing_API_keys)
 
   existing_API_keys.push(API_KEY)
   console.log('existing_API_keys', existing_API_keys)
