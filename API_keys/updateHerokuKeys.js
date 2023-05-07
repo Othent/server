38994e4918a3a337d121160f62ad5ebf5f8335ac
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-05-07 16:35:44 -0400

    -

--- API_keys/updateHerokuKeys.js
+++ API_keys/updateHerokuKeys.js
@@ -12,6 +12,7 @@ export default async function updateHerokuKeys(API_KEY, API_ID) {
   const existing_API_keys = process.env.API_KEYS ? process.env.API_KEYS.split(',') : [];
 
   console.log('existing_API_keys', existing_API_keys)
+  console.log(typeof process.env.API_KEYS)
 
   existing_API_keys.push(API_KEY)
   console.log('existing_API_keys', existing_API_keys)
