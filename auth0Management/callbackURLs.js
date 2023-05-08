f73d98ba9e8e8c63d20e1e87a62a3c3c47ac05e8
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-05-07 20:55:02 -0400

    feat: add callback URLs

---
+++ auth0Management/callbackURLs.js
@@ -0,0 +1,37 @@
+
+
+
+export default async function addCallbackURL(callbackURL) {
+
+
+    var auth0Domain = process.env.auth0Domain;
+    var auth0ClientId = process.env.auth0ClientId;
+    var auth0ClientSecret = process.env.auth0ClientSecret;
+    var audience = 'https://' + auth0Domain + '/api/v2/';
+    var tokenUrl = 'https://' + auth0Domain + '/oauth/token';
+    var tokenParams = {
+    grant_type: 'client_credentials',
+    client_id: auth0ClientId,
+    client_secret: auth0ClientSecret,
+    audience: audience
+    };
+
+    axios.post(tokenUrl, tokenParams)
+    .then(function (response) {
+        var token = response.data.access_token;
+
+        const apiUrl = `${audience}clients/dyegx4dZj5yOv0v0RkoUsc48CIqaNS6C`;
+        const headers = { 'Authorization': `Bearer ${token}`, 'Content-Type': 'application/json' };
+        const body = { callbacks: [ callbackURL ] };
+
+        axios.patch(apiUrl, body, { headers })
+        .then(response => {
+            return {response: true, message: `Successfully updated allowed URLs: ${response.data.callbacks}`}
+        })
+        .catch(error => {
+            return {response: false, message: `Error updating allowed URLs: ${error}`}
+        });   
+
+    })
+    
+  }
