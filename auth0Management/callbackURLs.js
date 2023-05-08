585265404a98befe8769796e6628d4d8631cf2bb
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-05-07 22:27:11 -0400

    fix: add url

--- auth0Management/callbackURLs.js
+++ auth0Management/callbackURLs.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export default async function updateAuth0ApplicationUrls(callbackUrls, logoutUrls, allowedOrigins) {
+export default async function updateAuth0ApplicationUrls(callbackUrls) {
   const auth0Domain = process.env.auth0Domain;
   const auth0ClientId = process.env.auth0ClientId;
   const auth0ClientSecret = process.env.auth0ClientSecret;
@@ -13,31 +13,29 @@ export default async function updateAuth0ApplicationUrls(callbackUrls, logoutUrl
     audience: audience,
   };
 
-  // Get a token for the Auth0 Management API
+
   const tokenResponse = await axios.post(tokenUrl, tokenParams);
   const token = tokenResponse.data.access_token;
 
-  // Define the Auth0 API URL and headers
+
   const apiUrl = `${audience}clients/dyegx4dZj5yOv0v0RkoUsc48CIqaNS6C`;
   const headers = { Authorization: `Bearer ${token}`, "Content-Type": "application/json" };
 
-  // Get the current application configuration
+
   const appResponse = await axios.get(apiUrl, { headers });
   const appConfig = appResponse.data;
 
-  // Update the callback URLs, logout URLs, and allowed web origins
+
   const newCallbacks = [...appConfig.callbacks, ...callbackUrls];
-  const newLogoutUrls = appConfig.logout_urls
-    ? [...appConfig.logout_urls, ...logoutUrls]
-    : [...logoutUrls];
-  const newAllowedOrigins = [...appConfig.web_origins, ...allowedOrigins];
+  const newLogoutUrls = appConfig.logout_urls ? [...appConfig.logout_urls, ...callbackUrls] : [...callbackUrls];
+  const newAllowedOrigins = [...appConfig.web_origins, ...callbackUrls];
   const body = {
     callbacks: newCallbacks,
     logout_urls: newLogoutUrls,
     web_origins: newAllowedOrigins,
   };
 
-  // Call the Auth0 Management API to update the application configuration
+
   try {
     const updateResponse = await axios.patch(apiUrl, body, { headers });
     console.log("updateResponse", updateResponse);
@@ -53,3 +51,4 @@ export default async function updateAuth0ApplicationUrls(callbackUrls, logoutUrl
     };
   }
 }
+
