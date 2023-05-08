2157c98ab693845383d1d8edf5a28792d29a7939
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-05-07 22:15:08 -0400

    fix: callback URLS

--- auth0Management/callbackURLs.js
+++ auth0Management/callbackURLs.js
@@ -1,42 +1,53 @@
 import axios from "axios";
 
-
-export default async function addCallbackURL(callbackURL) {
-
-    var auth0Domain = process.env.auth0Domain;
-    var auth0ClientId = process.env.auth0ClientId;
-    var auth0ClientSecret = process.env.auth0ClientSecret;
-    var audience = 'https://' + auth0Domain + '/api/v2/';
-    var tokenUrl = 'https://' + auth0Domain + '/oauth/token';
-    var tokenParams = {
-    grant_type: 'client_credentials',
+export default async function updateAuth0ApplicationUrls(callbackUrls, logoutUrls, allowedOrigins) {
+  const auth0Domain = process.env.auth0Domain;
+  const auth0ClientId = process.env.auth0ClientId;
+  const auth0ClientSecret = process.env.auth0ClientSecret;
+  const audience = `https://${auth0Domain}/api/v2/`;
+  const tokenUrl = `https://${auth0Domain}/oauth/token`;
+  const tokenParams = {
+    grant_type: "client_credentials",
     client_id: auth0ClientId,
     client_secret: auth0ClientSecret,
-    audience: audience
+    audience: audience,
+  };
+
+
+  const tokenResponse = await axios.post(tokenUrl, tokenParams);
+  const token = tokenResponse.data.access_token;
+
+
+  const apiUrl = `${audience}clients/dyegx4dZj5yOv0v0RkoUsc48CIqaNS6C`;
+  const headers = { Authorization: `Bearer ${token}`, "Content-Type": "application/json" };
+
+
+  const appResponse = await axios.get(apiUrl, { headers });
+  const appConfig = appResponse.data;
+
+
+  const newCallbacks = [...appConfig.callbacks, ...callbackUrls];
+  const newLogoutUrls = [...appConfig.logout_urls, ...logoutUrls];
+  const newAllowedOrigins = [...appConfig.web_origins, ...allowedOrigins];
+  const body = {
+    callbacks: newCallbacks,
+    logout_urls: newLogoutUrls,
+    web_origins: newAllowedOrigins,
+  };
+
+
+  try {
+    const updateResponse = await axios.patch(apiUrl, body, { headers });
+    console.log("updateResponse", updateResponse);
+    return {
+      success: true,
+      message: `Successfully updated application URLs`,
     };
-
-    console.log(123)
-
-    axios.post(tokenUrl, tokenParams)
-    .then(function (response) {
-        var token = response.data.access_token;
-
-        const apiUrl = `${audience}clients/dyegx4dZj5yOv0v0RkoUsc48CIqaNS6C`;
-        const headers = { 'Authorization': `Bearer ${token}`, 'Content-Type': 'application/json' };
-        const body = { callbacks: [ callbackURL ] };
-
-        axios.patch(apiUrl, body, { headers })
-        .then(response => {
-            console.log('response', response)
-            return {response: true, message: `Successfully updated allowed URLs: ${response.data.callbacks}`}
-        })
-        .catch(error => {
-            return {response: false, message: `Error updating allowed URLs: ${error}`}
-        });   
-
-    })
-    .catch(error => {
-        return {response: false, message: `Error updating allowed URLs: ${error}`}
-    });   
-    
+  } catch (error) {
+    console.error("updateError", error);
+    return {
+      success: false,
+      message: `Error updating application URLs: ${error}`,
+    };
   }
+}
