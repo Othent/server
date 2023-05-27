5733eeeacec4475f1c4ddce5449ab18489b9da03
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-05-27 06:20:07 -0400

    -

--- index.js
+++ index.js
@@ -156,7 +156,9 @@ app.post('/read-contract', (req, res) => {
 // Upload data - arweave
 import uploadFileToArweave from './arweave/upload.js';
 app.post('/upload-data-arweave', upload.single('file'), (req, res) => {
-  const data = req.data;
+  console.log('req', req)
+  console.log('body', body)
+  const data = req.body.data;
   const dataHashJWT = req.body.dataHashJWT;
   const tags = JSON.parse(req.body.tags);
   const clientID = req.body.API_ID
