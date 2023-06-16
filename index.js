5441b9a8d5d152854ab67f09eeda36e0b1728807
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-06-16 01:54:41 +0100

    feat: query a users TXNs by their wallet address

--- index.js
+++ index.js
@@ -70,6 +70,22 @@ app.post('/query-client-id', (req, res) => {
 
 
 
+// Query a users wallet address for thier TXNs
+import queryClient from './patnerDashboard/queryWalletAddress.js';
+app.post('/query-wallet-address', (req, res) => {
+  const walletAddress = req.body.walletAddress;
+  queryWalletAddress(walletAddress)
+  .then((response) => {
+    res.json(response);
+  })
+  .catch((error) => {
+    res.status(500).json({ success: false, error: error.message });
+  });
+});
+
+
+
+
 
 
 // Save email 
---
+++ patnerDashboard/queryWalletAddress.js
@@ -0,0 +1,44 @@
+import { MongoClient } from 'mongodb';
+
+async function queryWalletAddress(walletAddress) {
+  try {
+    const username = process.env.mongoDBUsername;
+    const password = encodeURIComponent(process.env.mongoDBPassword);
+    const hosts = process.env.mongoDBHost;
+    const database = process.env.mongoDBName;
+    const connectionString = `mongodb://${username}:${password}@${hosts}/${database}`;
+    const client = new MongoClient(connectionString);
+
+    await client.connect();
+    const db = client.db();
+    const collection = db.collection('clients');
+    const clients = await collection.find({}).toArray();
+    await client.close();
+
+    let combinedTransactions = [];
+    clients.forEach(client => {
+        const { clientID, transactions } = client;
+        transactions.forEach(transaction => {
+        const transactionWithClientID = { ...transaction, ClientID: clientID };
+        combinedTransactions.push(transactionWithClientID);
+        });
+    });
+
+    combinedTransactions.sort((a, b) => a.date - b.date);
+
+    const findTransactionsByWalletAddress = (walletAddress) => {
+      return combinedTransactions.filter((transaction) => {
+        return transaction.walletAddress === walletAddress;
+      });
+    };
+
+    const transactions = findTransactionsByWalletAddress(walletAddress);
+
+    return { success: true, transactions: transactions };
+  } catch (error) {
+    return { success: false, error: error.message };
+  }
+}
+
+
+export default queryWalletAddress;
