4f4968a0483bc70290c84c60a4f32c8176d990f7
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-07-07 19:44:15 +0100

    feat: transfer U backend, with test TXN

---
+++ claimU/sendUWarp.js
@@ -0,0 +1,17 @@
+import { warp, configureWallet } from "../warp/warp-configs";
+
+
+export default async function transferU(contract_id) {
+
+    const connectedWallet = warp.contract('KTzTXT_ANmF84fWEKHzWURD1LWd9QaFR9yfYUwH2Lxw').connect(configureWallet.jwk);
+
+    const transfer = await connectedWallet.writeInteraction({
+        function: 'transfer',
+        target: contract_id,
+        qty: 100000 // transfer 0.1 
+    });
+
+    console.log(transfer)
+
+    return { success: true }
+}
