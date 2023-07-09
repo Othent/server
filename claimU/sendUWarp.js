98b5f30cd42c581bdd6f32f547fb29bee4384735
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-07-09 19:03:05 +0100

    fix: configWallet

--- claimU/sendUWarp.js
+++ claimU/sendUWarp.js
@@ -3,7 +3,8 @@ import { warp, configureWallet } from "../warp/warp-configs.js";
 
 export default async function transferU(contract_id) {
 
-    const connectedWallet = warp.contract('KTzTXT_ANmF84fWEKHzWURD1LWd9QaFR9yfYUwH2Lxw').connect(configureWallet.jwk);
+    const wallet = await configureWallet();
+    const connectedWallet = warp.contract('KTzTXT_ANmF84fWEKHzWURD1LWd9QaFR9yfYUwH2Lxw').connect(wallet.jwk);
 
     const transfer = await connectedWallet.writeInteraction({
         function: 'transfer',
