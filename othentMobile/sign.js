ff7025eff5d6484ca6d8cd5991b37b66780af968
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-08-04 19:11:18 +0100

    feat: sign function

--- othentMobile/sign.js
+++ othentMobile/sign.js
@@ -1,9 +1,23 @@
+import jwkToPem from 'jwk-to-pem';
+import crypto from 'crypto';
 
 
+async function sign(data) {
 
-async function sign(message) {
+    const pemKey = jwkToPem(process.env.wallet, { private: true });
 
-    return message
+    const sign = crypto.createSign('sha256');
+    sign.update(data);
+    sign.end();
+
+    const signature = sign.sign({
+        key: pemKey,
+        padding: crypto.constants.RSA_PKCS1_PSS_PADDING,
+        saltLength: 15
+    });
+
+
+    return signature
 
 }
 
