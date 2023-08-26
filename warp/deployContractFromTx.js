985fe091a33e55fc0c9e874c7d1fa8f963b32ea4
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-08-26 20:56:27 +0100

    feat: deployContractFromTx

---
+++ warp/deployContractFromTx.js
@@ -0,0 +1,28 @@
+import { warp as warpFunction, configureWallet } from './warp-configs.js'
+import jwt from 'jsonwebtoken';
+
+
+export default async function deployContractFromTx(network, srcTxId, contractState, JWT, tags) {
+
+
+    const decodedJWT = jwt.decode(JWT)
+
+    if (srcTxId !== decodedJWT.srcTxId) {
+        return { success: false, error: 'Invalid JWT/ContractTx' }
+    }
+
+    const wallet = await configureWallet();
+    tags.push( { name: "File-Hash-JWT", value: JWT } )
+
+
+    const warp = await warpFunction(network)
+    const contractResponse = await warp.deployFromSourceTx({
+        wallet: wallet, 
+        initState: JSON.stringify(contractState), 
+        srcTxId: srcTxId,
+        tags
+    });
+
+
+    return contractResponse
+}
